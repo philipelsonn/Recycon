12220f60816603c107d9f28551616f15f78ec58a
Hans <hansnathanaeljr@gmail.com>
2022-12-29 15:36:25 +0700

    update tampilan

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -6,15 +6,15 @@
     <p class="fs-2 text-dark mt-3 mb-3 d-flex justify-content-center">Our Products</p>
     <div class="row d-flex justify-content-center">
     @foreach ($products as $product)
-        <div class="card col-md-4 ms-4 me-4" style="width: 25rem;">
+        <div class="card col-md-4 ms-4 me-4 border border-warning" style="width: 25rem;">
             <img src="/storage/images/item/{{ $product->image }}" class="card-img-top p-2" style="height: 400px" alt="...">
             <div class="card-body ">
                 <div class="d-flex justify-content-between">
                     <h5 class="card-title">{{ $product->name }}</h5>
                     <h5>{{ $product->category }}</h5>
                 </div>
-                <p class="card-text">{{ $product->price }}</p>
-                <a href="/showProduct/{{ $product->item_id }}" class="btn btn-primary bg-warning">Detail Product</a>
+                <p class="card-text">IDR. {{ $product->price }}</p>
+                <a href="/showProduct/{{ $product->item_id }}" class="btn btn-primary bg-warning text-dark border border-warning">Detail Product</a>
             </div>
         </div>
     @endforeach
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 </head>
-<body class="d-flex flex-column min-vh-100">
+<body class="d-flex flex-column min-vh-100 bg-light">
     <nav class="navbar navbar-expand-lg bg-primary">
         <div class="container-fluid">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
