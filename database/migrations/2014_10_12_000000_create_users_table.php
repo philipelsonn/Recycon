e4ac88f72dd758b82ca88044dd7770e95d42ba93
philipelsonn <83831775+philipelsonn@users.noreply.github.com>
2022-12-29 19:34:36 +0700

    authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,7 +10,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('type');
+            $table->string('type')->default('USER');
             $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\ItemController;
 Route::get('/showProduct', [ItemController::class, 'showProduct']);
 Route::get('/showProduct/{item_id?}', [ItemController::class, 'showDetail']);
 Route::resource('items', ItemController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
