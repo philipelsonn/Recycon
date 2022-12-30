16350e3543191938f467a0f2045475c3920ada30
philipelsonn <83831775+philipelsonn@users.noreply.github.com>
2022-12-30 13:09:49 +0700

    fix home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,25 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('home');
     }
-    
-    // public function guestHome(){
-    //     return view('homepage.home');
-    // }
-
-    // public function adminHome(){
-    //     return view('homepage.admin');
-    // }
-
-    // public function userHome(){
-    //     return view('homepage.user');
-    // }
 }
