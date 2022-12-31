a4eb371a10337f6d91ef40a6791c6964e7ac23af
Hans <hansnathanaeljr@gmail.com>
2022-12-31 08:41:49 +0700

    login register

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -20,56 +20,55 @@
                     <li class="nav-item">
                         <a class="nav-link text-light" href="/showProduct">Show Product</a>
                     </li>
-                    @if (auth()->user())
+                    @auth
                         @if (auth()->user()->type == 'USER')
-                        <li class="nav-item dropdown">
-                            <a class="nav-link text-light" href="/showProduct">My Cart</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link text-light" href="/showProduct">Transaction History</a>
-                        </li>
-                        @elseif (auth()->user()->type == 'ADMIN')
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Manage Item
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item text-dark" href="#">View Item</a></li>
-                                <li><a class="dropdown-item text-dark" href="#">Add Item</a></li>
-                            </ul>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="">My Cart</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="">Transaction History</a>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <div class="nav-item dropdown me-3">
+                                    <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Manage Item
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item text-dark" href="#">View Item</a></li>
+                                        <li><a class="dropdown-item text-dark" href="#">Add Item</a></li>
+                                    </ul>
+                                </div>
+                            </li>
                         @endif
-                    @endif
+                    @endauth
                 </ul>
-                @if (auth()->user())
-                <form class="d-flex w-50" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search product..." aria-label="Search">
-                    <button class="btn btn-outline-light" type="submit">Search</button>
-                </form>
-                <div class="nav-item dropdown ms-3 me-3">
-                    <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Profile
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item text-dark" href="#">Edit Profile</a></li>
-                        <li><a class="dropdown-item text-dark" href="#">Change Password</a></li>
-                    </ul>
-                </div>
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
+                @auth
+                    <form class="d-flex w-50" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search product..." aria-label="Search">
+                        <button class="btn btn-outline-light" type="submit">Search</button>
+                    </form>
+                    <div class="nav-item dropdown ms-3 me-3">
+                        <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Profile
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="d-flex justify-content-center">{{ auth()->user()->username }}</li>
+                            <hr>
+                            <li><a class="dropdown-item text-dark" href="#">Edit Profile</a></li>
+                            <li><a class="dropdown-item text-dark" href="#">Change Password</a></li>
+                        </ul>
+                    </div>
+                    <form action="/logout" method="post">
+                        @csrf
+                        <button type="submit" class="btn btn-outline-light">Logout</button>
+                    </form>
                 @else
-                <form class="d-flex" role="search">
-                    <a class="btn btn-outline-light" type="submit" href="{{ route('login') }}">Login</a>
-                    <a class="btn btn-outline-light ms-2" type="submit" href="{{ route('register') }}">Register</a>
-                </form>
-                @endif
+                    <form class="d-flex" role="search">
+                        <a class="btn btn-outline-light" type="submit" href="/login">Login</a>
+                        <a class="btn btn-outline-light ms-2" type="submit" href="/register">Register</a>
+                    </form>
+                @endauth
             </div>
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Auth;
 
-Auth::routes();
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/home', [HomeController::class, 'index']);
-
+Route::get('/home', [HomeController::class, 'home']);
 Route::get('/showProduct', [ItemController::class, 'showProduct']);
 Route::get('/showProduct/{item_id?}', [ItemController::class, 'showDetail']);
 Route::resource('items', ItemController::class);
-
+Route::get('/login', [AuthController::class, 'login'])->middleware('guest');
+Route::post('/login', [AuthController::class, 'loginAuth']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/register', [AuthController::class, 'register'])->middleware('guest');
+Route::post('/register', [AuthController::class, 'registerAuth']);
 
