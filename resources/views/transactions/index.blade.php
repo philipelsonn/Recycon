01d0cb638ba5290fb84f70c37b78fc1b9dcb62db
philipelsonn <83831775+philipelsonn@users.noreply.github.com>
2023-01-03 15:28:27 +0700

    add: transaction date filter

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -12,6 +12,8 @@
         </div>
     @elseif (count($transactions) > 0)
         <div class="container-fluid">
+            @foreach ($dates as $date)
+            <h4 class="text-dark fw-bold ms-4 mt-3">{{ $date->format('d:m:Y') }}</h4>
             <div class="table-responsive px-3">
                 <table class="table table-bordered table-sm table-striped no-footer table-warning border-primary">
                     <thead class="thead-light">
@@ -28,26 +30,27 @@
                         @php($grandTotal = 0)
                         @php ($i = 1)
                         @foreach ($transactions as $transaction)
+                        @if ($transaction->created_at == $date)
                         @php($total = $transaction->item->price * $transaction->quantity)
-                            <tr class="text-center">
-                                <td class="align-middle">{{ $i }}</td>
-                                <td class="align-middle"><img src="/storage/images/item/{{ $transaction->item->image }}" alt="" style="height: 70px; width: 100px"></td>
-                                <td class="align-middle">{{ $transaction->item->name }}</td>
-                                <td class="align-middle">{{ $transaction->item->price }}</td>
-                                <td class="align-middle">{{ $transaction->quantity }}</td>
-                                <td class="align-middle">{{ $total }}</td>
-                            </tr>
+                        <tr class="text-center">
+                            <td class="align-middle">{{ $i }}</td>
+                            <td class="align-middle"><img src="/storage/images/item/{{ $transaction->item->image }}" alt="" style="height: 70px; width: 100px"></td>
+                            <td class="align-middle">{{ $transaction->item->name }}</td>
+                            <td class="align-middle">{{ $transaction->item->price }}</td>
+                            <td class="align-middle">{{ $transaction->quantity }}</td>
+                            <td class="align-middle">{{ $total }}</td>
+                        </tr>
                         @php ($i = $i + 1)
                         @php($grandTotal = $grandTotal + $total)
+                        @endif
                         @endforeach
                     </tbody>
                 </table>
             </div>
+            <div class="container-fluid">
+                <h4 class="text-dark fw-bold ms-4 mt-3">Grand total: {{ $grandTotal }}</h4>
+            </div>
         </div>
-    @endif
-    @if (count($transactions) > 0)
-    <div class="container-fluid">
-        <h4 class="text-dark fw-bold ms-4 mt-3">Grand total: {{ $grandTotal }}</h4>
-    </div>
+        @endforeach
     @endif
 @endsection
