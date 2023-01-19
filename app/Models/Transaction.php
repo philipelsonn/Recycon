3c8a6ca662b71406b7c3d39be255a95e75ef66fa
philipelsonn <83831775+philipelsonn@users.noreply.github.com>
2023-01-19 16:59:58 +0700

    FINAL

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -15,7 +15,7 @@ class Transaction extends Model
 
     public function user()
     {
-        return $this->hasOne('App\Models\User', 'id', 'user_id');
+        return $this->belongsTo('App\Models\User', 'user_id', 'id');
     }
 
     public function item()
